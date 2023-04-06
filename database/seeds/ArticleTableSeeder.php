1fb2a0623b12aefcd3233d342a709d0f2b063193
HanNG3 <hanng3@fpt.com.vn>
2023-04-06 16:09:53 +0700

    init

---
+++ database/seeds/ArticleTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Article;
+use Illuminate\Database\Seeder;
+
+class ArticleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for($i=0; $i<50; $i++) {
+          Article::create([
+            'title' => $faker->sentence(3),
+            'body' => $faker->paragraph(6),
+            'tags' => join(',', $faker->words(4))
+          ]);
+        }
+    }
+}
